a24dd60f33b75c6ad27f26bee6e7bfa93e19d783
Bilal Asghar <bilalasghar.contact@gmail.com>
2026-03-20 01:50:00 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // char array mein naam store karna
+    char *name = "Bilal Asghar";
+
+    // %s ka istemal string print karne ke liye
+    printf("Naam: %s\n", name);
+
+    return 0;
+}
